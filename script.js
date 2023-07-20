cbb01fe285d083d37ac3613f46f7157411ccaf7b
Krystle Lee <l33.krys@gmail.com>
2023-07-19 17:08:59 -0700

    create button, add javascript functionality

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 /* const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2); */
+
+const button = document.getElementById('btn');
+button.addEventListener("click", function() {
+    document.getElementById('answer').innerHTML = "Surprise!";
+    document.getElementById('answer').style.visibility = "visible";
+})
+
+
