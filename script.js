fde47900cd26ac048ce0b0db2015fd74ed1ed945
Krystle Lee <l33.krys@gmail.com>
2023-07-19 22:35:37 -0700

    toggle view and hide menu

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2); */
 
+// Today's meal
 const button = document.getElementById('btn');
 button.addEventListener("click", function() {
     document.getElementById('answer').innerHTML = daily_meal;
@@ -23,3 +24,22 @@ const meals = {
 let day_position = new Date().getDay(); //3
 let test = day_of_week[day_position];
 let daily_meal = meals[test];
+
+// Toggle full menu from visible to hidden
+const full_menu = document.getElementById('toggle');
+full_menu.addEventListener("click", function() {
+    if (full_menu.innerHTML === "View full menu") {
+        full_menu.innerHTML = "Hide full menu";
+        const menu_meals = document.getElementsByClassName('menu');
+        for (const dish of menu_meals) {
+            dish.style.visibility = 'visible';
+        }
+    } else if (full_menu.innerHTML === "Hide full menu") {
+        full_menu.innerHTML = "View full menu";
+        const menu_meals = document.getElementsByClassName('menu');
+        for (const dish of menu_meals) {
+            dish.style.visibility = 'hidden';
+        }    
+    }
+}
+)
