784ec6301ab6e3cda1a28c78f5e967f28cd09aa9
Krystle Lee <l33.krys@gmail.com>
2023-07-19 20:50:11 -0700

    update javascript to show correct daily meal

--- script.js
+++ script.js
@@ -4,8 +4,22 @@ document.querySelector("body").appendChild(h2); */
 
 const button = document.getElementById('btn');
 button.addEventListener("click", function() {
-    document.getElementById('answer').innerHTML = "Surprise!";
+    document.getElementById('answer').innerHTML = daily_meal;
     document.getElementById('answer').style.visibility = "visible";
 })
+const day_of_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
+const meals = {
+    Sunday: "Baked Spaghetti",
+    Monday: "Carnitas Tacos",
+    Tuesday: "Sweet and Sour Shrimp",
+    Wednesday: "Stuffed Salmon",
+    Thursday: "Leftovers",
+    Friday: "DoorDash",
+    Saturday: "Tri Tip",
+}
 
+// let date = new Date();
+let day_position = new Date().getDay(); //3
+let test = day_of_week[day_position];
+let daily_meal = meals[test];
